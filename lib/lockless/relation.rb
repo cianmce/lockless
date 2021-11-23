ac243b545c4b2c3749ffa7aa8a75eb0a2fba33e7
Cian McElhinney <cianmce@gmail.com>
2021-11-23 15:07:32 +0000

    update_all only if param is a hash

--- lib/lockless/relation.rb
+++ lib/lockless/relation.rb
@@ -2,9 +2,20 @@
 
 module Lockless
   module Relation
+
+    # Appends updated attribute of a random UUID to each update command
+    # when the model is a lockless model
+    #
+    # @param [String, Array, Hash] A string, array, or hash representing the SET part of an SQL statement.
+    # Lockless will only append random UUID to updates if the param is a Hash
+    #
+    # @return [Boolean] Similar to `.save`, true is returned if the model is updated
+    # false is returned if record is outdated or invalid
     def update_all(updates)
-      if model.method_defined?(:lockless_column) && updates[model.lockless_column].blank?
-        updates[model.lockless_column] = model.generate_uuid
+      if updates.is_a?(Hash)
+        if model.method_defined?(:lockless_column) && updates[model.lockless_column].blank?
+          updates[model.lockless_column] = model.generate_uuid
+        end
       end
 
       super(updates)
